3f181482fefc577abfc39c7d89c1770b07fcc3de
Ewata Efe Faith <ewataefe721@gmail.com>
2026-05-05 08:34:14 +0100

    change

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
-// function App() {
-  
-//   return (
-//     <>
-      
-//     </>
-//   )
-// }
+import { MusicPlayer } from "./Components/MusicPlayer"
 
-// export default App
+function App() {
+  
+  return (
+    <div className="app">
+        {/* <Navbar /> */}
+        <main className="app-main">
+            <div className="player-section">
+                <MusicPlayer />
+            </div>
+            <div className="content-section">
+                
+            </div>
+        </main>
+    </div>
+  )
+}
+
+export default App
 
